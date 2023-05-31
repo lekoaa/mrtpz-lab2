257887e65ed6ff8294da8c93c5b2c287f560bb2a
Andriyaschenko Vladimir <vovan4ik041@gmail.com>
2023-05-31 19:25:37 +0300

    Update main.py
    I noticed that I didn't do the task according to the condition

--- main.py
+++ main.py
@@ -3,22 +3,48 @@ from typed_list import TypedList
 
 def main():
 
-    my_list = TypedList()
+    lst = TypedList()
 
-    my_list.add(10)
-    my_list.add(20)
-    my_list.add(30)
+    print("Initial Length:", lst.length())
 
-    print(f"Size of the list: {my_list.size()}")
+    lst.append('A')
+    lst.append('B')
+    lst.append('C')
 
-    item = my_list.get(1)
-    if item is not None:
-        print(f"Item at index 1: {item}")
+    print("Length after appending:", lst.length())
 
-    my_list.remove(20)
+    lst.insert('X', 1)
+    lst.insert('Y', 3)
 
-    print(f"Size of the list after removal: {my_list.size()}")
+    print("List after insertion:", lst)
 
+    deleted_item = lst.delete(2)
+    print("Deleted item:", deleted_item)
+    print("List after deletion:", lst)
+
+    lst.deleteAll('B')
+    print("List after deleting all 'B':", lst)
+
+    item = lst.get(0)
+    print("Item at index 0:", item)
+
+    cloned_list = lst.clone()
+    print("Cloned list:", cloned_list)
+
+    lst.reverse()
+    print("Reversed list:", lst)
+
+    index = lst.findFirst('X')
+    print("Index of first 'X':", index)
+
+    index = lst.findLast('Y')
+    print("Index of last 'Y':", index)
+
+    lst.clear()
+    print("List after clearing:", lst)
+
+    lst.extend(['D', 'E', 'F'])
+    print("List after extending:", lst)
 
 if __name__ == "__main__":
     main()
